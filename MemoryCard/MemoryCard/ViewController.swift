b418b6f1831688c45ecb81d92dfbd5053e69ab42
Anton Khlopin <khlopin_ant@mail.ru>
2023-09-12 19:59:53 +0500

    New project

---
+++ MemoryCard/MemoryCard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemoryCard
+//
+//  Created by Anton Khlopin on 12.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
